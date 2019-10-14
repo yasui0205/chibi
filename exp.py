2ed77e55119ba025a833e60ea7068b3a46712542
YASUI Yuka <21716098@jwu.ac.jp>
2019-10-14 16:29:01 +0900

    Expr

--- exp.py
+++ exp.py
@@ -1,6 +1,9 @@
 
-class Val(object):
-    __slots__=['value']
+class Expr(object):
+    pass
+
+class Val(Expr):
+    __slots__ = ['value']
     def __init__(self, value = 0):
         self.value = value
     def __repr__(self):
@@ -8,10 +11,31 @@ class Val(object):
     def eval(self):
         return self.value
 
-        
 v = Val(1)
 print(v)
 assert v.eval() == 1
+
+assert isinstance(v, Expr)
+assert isinstance(v, Val)
+assert isinstance(v, int)
+
+class Add(Expr):
+    __slots__=['left', 'right']
+    def __init_(self, a, b):
+        self.left = a
+        self.right = b
+    def eval(self):
+        return self.left.eval() + self.right.eval()
+
+e = Add(Val(1), Val(2))
+assert e.eval() == 3
+
+e = Add(Val(1),Add(Val(2),Val(3)))
+assert e.eval() == 6
+
+
+e = Add(Val(1),Add(Val(2),Val(3)))
+assert e.eval() == 6
 
 
 
